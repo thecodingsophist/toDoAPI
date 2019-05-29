36ddcf48f4a43d6bb54a4280d7f25a7e05baa947
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-05-28 22:17:53 -0700

    added get all route and defined PORT

---
+++ db/db.js
@@ -0,0 +1,14 @@
+const todos = [
+    {
+        id: 1,
+        title: "lunch",
+        description: "Go for lunch by 2pm"
+    },
+    {
+        id: 2,
+        title: "code",
+        description: "debug by 4pm"
+    }
+];
+
+export default todos;
