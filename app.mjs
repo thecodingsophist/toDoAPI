cd9e38c40f71c3f9ecf108af10601d4a9b1b2d25
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-05-29 01:17:02 -0700

    added a POST route

--- app.mjs
+++ app.mjs
@@ -1,6 +1,7 @@
 // todo tutorial import method
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
 
 // online solution express import method
 // const express = require('express', '4.17.1')
@@ -10,9 +11,18 @@ import db from './db/db';
 
 const app = express();
 
+//Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.send('What\'s up?')
 })
+
+// First Test
+// app.get('/', (req, res) => {
+//     res.send('Hello Word!')
+// })
 
 app.get('/api/v1/todos', (req, res) => {
     res.status(200).send({
@@ -22,10 +32,30 @@ app.get('/api/v1/todos', (req, res) => {
     })
 });
 
-// First Test
-// app.get('/', (req, res) => {
-//     res.send('Hello Word!')
-// })
+app.post('/api/v1/todos', (req, res) => {
+    if(!req.body.title) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    } else if(!req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+    const todo = {
+        id: db.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    db.push(todo);
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
+    })
+});
 
 const PORT = 3000;
 
