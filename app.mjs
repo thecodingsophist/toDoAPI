bea942830a7e33de1a5a8fef8254103464f322c0
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-05-29 09:48:40 -0700

    added UPDATE route

--- app.mjs
+++ app.mjs
@@ -74,6 +74,49 @@ app.get('/api/v1/todos/:id', (req, res) => {
     });
 });
 
+app.put('/api/v1/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    let todoFound;
+    let itemIndex;
+    db.map((todo, index) => {
+        if (todo.id === id) {
+            todoFound = todo;
+            itemINdex = index;
+        }
+    });
+    if (!todoFound) {
+        return res.status(404).send({
+            success: 'false',
+            message: 'todo not found',
+        });
+    }
+    if (!req.body.title) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required',
+        });
+        else if (!req.body.description) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'description is required',
+            });
+        }
+        const updatedTodo = {
+            id: todoFound.id,
+            title: req.body.title || todoFound.title,
+            description: req.body.description || todoFound.description,
+        };
+
+        db.splice(itemIndex, 1, updatedTodo);
+
+        return res.status(201).send({
+            success: 'true',
+            message: 'todo added successfully',
+            updatedTodo
+        });
+    };
+})
+
 app.delete('/api/v1/todos/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
 
