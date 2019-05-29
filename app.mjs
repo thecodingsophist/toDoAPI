20b685c2708e6367d2c209e8dc9a93d4e7bbd219
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-05-29 01:35:15 -0700

    added GET by id and DELETE routes

--- app.mjs
+++ app.mjs
@@ -57,6 +57,42 @@ app.post('/api/v1/todos', (req, res) => {
     })
 });
 
+app.get('/api/v1/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    db.map((todo) => {
+        if (todo.id === id) {
+            return res.status(200).send({
+                success: 'true',
+                message: 'todo retrieved successfully',
+                todo,
+            });
+        }
+    });
+    return res.status(404).send({
+        success: 'false',
+        message: 'todo does not exist',
+    });
+});
+
+app.delete('/api/v1/todos/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    db.map((todo, index) => {
+        if (todo.id === id) {
+            db.splice(index, 1);
+            return res.status(200).send({
+                success: 'true',
+                message: 'Todo deleted successfully',
+            });
+        }
+    });
+
+    return res.status(404).send({
+        success: 'false',
+        message: 'todo not found',
+    });
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
